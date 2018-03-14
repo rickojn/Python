df6630c01e242deb5ac0a475a57a07e5cf9dd1a4
unknown <rickojn@hotmail.com>
2018-03-14 22:49:50 +0000

    evaluation

---
+++ validate_poi.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+
+"""
+    Starter code for the validation mini-project.
+    The first step toward building your POI identifier!
+
+    Start by loading/formatting the data
+
+    After that, it's not our code anymore--it's yours!
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+from sklearn import tree
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+features_train, features_test, labels_train, labels_test = train_test_split(features,labels, test_size = 0.3, random_state = 0)
+
+
+### it's all yours from here forward!  
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+score = accuracy_score(labels_test,pred)
+
+print pred
+print pred.size
+print "accuracy = ",score
+
+print labels_test
+
+accurate_count = 0
+accurate_count_if_zero = 0
+for label_test, predItem in zip(labels_test,pred):
+    print "test label: ", label_test, "pred label: ", predItem
+    if label_test == predItem:
+        print "accurate"
+        accurate_count = accurate_count + 1 
+    if label_test == 0:
+        accurate_count_if_zero = accurate_count_if_zero + 1
+print "accurate_count: ", accurate_count
+print "accurate_count_if_zero: ", accurate_count_if_zero
+print "accuracy_if_zero: ", float(accurate_count_if_zero) / 29.0
+
+
+
